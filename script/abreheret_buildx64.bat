3bd5ce7858014bd04fad7d2935ab6b38b33af78f
Amaury Bréhéret <abreheret@gmail.com>
2016-07-05 11:22:57 +0200

    [CMakeLists] fixbug x64 arch

--- script/abreheret_buildx64.bat
+++ script/abreheret_buildx64.bat
@@ -1,4 +1,4 @@
 mkdir x64
 cd x64
-cmake -DQT5_DIR=D:/dev/Qt/5.7/msvc2013/lib/cmake -G "Visual Studio 12 Win64" ../..
+cmake -DQT5_DIR=D:/dev/Qt/5.7/msvc2013_64/lib/cmake -G "Visual Studio 12 Win64" ../..
 PAUSE
