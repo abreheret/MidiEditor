b5bfca6746808bdcd557365f948e310f590b74a4
Amaury Bréhéret <abreheret@gmail.com>
2016-07-06 19:32:34 +0200

    Remove force No Terminal in Win32 because Windobe

--- scripts_to_build_vc2013/installerx86.bat
+++ scripts_to_build_vc2013/installerx86.bat
@@ -1,6 +1,6 @@
 mkdir x86
 cd x86
-cmake -DNO_CONSOLE_MODE=ON -DINSTALL_WITH_CPACK=ON -G "Visual Studio 12" ../..
+cmake -DNO_CONSOLE_MODE=OFF -DINSTALL_WITH_CPACK=ON -G "Visual Studio 12" ../..
 cmake --build . --config Release
 cpack -DCPACK_GENERATOR=WIX -C Release
 PAUSE
