267eb4d6525d4e4eddf8640fc69ac4257a9bb955
Amaury Bréhéret <abreheret@gmail.com>
2016-07-06 19:12:35 +0200

    Fix bug : force No Terminal in Deployment

--- scripts_to_build_vc2013/abreheret_installerx86.bat
+++ scripts_to_build_vc2013/abreheret_installerx86.bat
@@ -1,6 +1,6 @@
 mkdir x86
 cd x86
-cmake -DQT5_DIR=D:/dev/Qt/5.7/msvc2013/lib/cmake -DINSTALL_WITH_CPACK=ON -G "Visual Studio 12" ../..
+cmake -DNO_CONSOLE_MODE=ON -DQT5_DIR=D:/dev/Qt/5.7/msvc2013/lib/cmake -DINSTALL_WITH_CPACK=ON -G "Visual Studio 12" ../..
 cmake --build . --config Release
 cpack -DCPACK_GENERATOR=WIX -C Release
 PAUSE
--- scripts_to_build_vc2013/installerx64.bat
+++ scripts_to_build_vc2013/installerx64.bat
@@ -1,6 +1,6 @@
 mkdir x64
 cd x64
-cmake -DINSTALL_WITH_CPACK=ON -G "Visual Studio 12 Win64" ../..
+cmake -DNO_CONSOLE_MODE=ON -DINSTALL_WITH_CPACK=ON -G "Visual Studio 12 Win64" ../..
 cmake --build . --config Release
 cpack -DCPACK_GENERATOR=WIX -C Release
 PAUSE
