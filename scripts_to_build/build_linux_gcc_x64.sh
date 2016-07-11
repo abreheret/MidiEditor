db0cf0c26eebbb7e3fafb46ac95ef69ac525573c
Amaury Bréhéret <abreheret@gmail.com>
2016-07-11 17:13:05 +0200

    script linux : add commande to generate version.h

--- scripts_to_build/build_linux_gcc_x64.sh
+++ scripts_to_build/build_linux_gcc_x64.sh
@@ -3,4 +3,6 @@ sudo apt-get install libsfml-dev
 sudo apt-get install libasound2-dev
 mkdir x64
 cd x64
+
 cmake -G "Unix Makefiles" ../..
+cmake -P version.cmake 
--- scripts_to_build/make_installer_linux_64.sh
+++ scripts_to_build/make_installer_linux_64.sh
@@ -2,5 +2,6 @@
 mkdir x64
 cd x64
 cmake -DINSTALL_WITH_CPACK=ON -G "Unix Makefiles" ../..
+cmake -P version.cmake 
 cmake --build . --config Release
 cpack -C Release
